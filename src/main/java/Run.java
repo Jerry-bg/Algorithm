d4cd0879d3b12b4af85565c455d289a25eda6df3
jerry <jerry975@126.com>
2019-10-30 13:58:32 +0800

    leetCode

---
+++ src/main/java/Run.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Run {
+    public static void main(String[] args){
+        List<List<Integer>> list=new ArrayList<>();
+        Queue<Integer> queue= new LinkedList<>();
+
+        queue.isEmpty();
+
+
+        List<Integer> list1=new ArrayList<>();
+        list1.add(1);
+
+        list.add(list1);
+        System.out.println(1+(4>>1));
+    }
+}
