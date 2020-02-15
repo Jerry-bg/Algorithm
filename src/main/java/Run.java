9cdd59482e362325e0c51d96005e15dab360845a
kons <jerry975@126.com>
2020-02-15 16:30:31 +0800

    add src

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,17 +1,25 @@
-import java.util.*;
+import model.TreeNode;
+import tree.LeetCode_107;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Run {
-    public static void main(String[] args){
-        List<List<Integer>> list=new ArrayList<>();
-        Queue<Integer> queue= new LinkedList<>();
-
-        queue.isEmpty();
-
-
-        List<Integer> list1=new ArrayList<>();
-        list1.add(1);
-
-        list.add(list1);
-        System.out.println(1+(4>>1));
+    public static void main(String[] args)  {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        InputStream reader = classLoader.getResourceAsStream("java.xml");
+        byte[] buff=new byte[1024];
+        try {
+            int n;
+            StringBuilder stringBuilder=new StringBuilder();
+            while ((n=reader.read())!=-1){
+                stringBuilder.append((char)n);
+            }
+            System.out.println(stringBuilder.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
